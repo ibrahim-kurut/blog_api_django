1909a8ef93d9bab7f5bbdc95f8d808cd2c07cf5e
ibrahim kurut <ibrahim.kurut06@gmail.com>
2024-08-24 22:19:14 +0300

    send msg for create & update & delete comments

--- blog/views.py
+++ blog/views.py
@@ -61,6 +61,46 @@ class BlogViewSet(ModelViewSet):
 class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+
+    # ============== send created msg ==============
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            {
+                'message': 'comment created successfully',
+                "data":serializer.data
+                }, 
+                status=status.HTTP_201_CREATED
+                )
+
+
+    # ============== send update msg ==============
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer) 
+        return Response(
+            {
+                'message': 'comment updated successfully',
+                "data":serializer.data
+                }, 
+                status=status.HTTP_200_OK
+                )
+
+    # ============== send deleted msg ==============
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {
+                'message': 'comment deleted successfully',
+                },status=status.HTTP_204_NO_CONTENT)
     
 
 class CategoryViewSet(ModelViewSet):
